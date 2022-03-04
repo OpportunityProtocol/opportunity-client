da252afbec0d0d7f2efc87473fc5f00fbcac7ea2
Elijah Hampton <elijah@ulap.co>
2022-03-04 15:11:29 -0600

    Initial commit

---
+++ src/pages/contract/view.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    Container,
+} from '@mui/material'
+import Blockies from 'react-blockies'
+
+import { useStyles } from './ContractStyles'
+import Link from 'next/link'
+
+const ViewContract: React.FunctionComponent<any> = () => {
+    const classes = useStyles()
+
+    return (
+        <Container maxWidth='lg'>
+            
+        </Container>
+    )
+}
+
+export default ViewContract
