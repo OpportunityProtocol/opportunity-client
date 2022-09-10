30c01bb0c57ffb04dfa61768f01413ee98ec0af3
elijahhampton <ejh0017@gmail.com>
2022-09-10 18:43:45 -0400

    Corrects reference to .env variable.

--- firebase.tsx
+++ firebase.tsx
@@ -14,7 +14,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLICP_APP_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
